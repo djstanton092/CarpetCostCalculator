84dd93c964a20b7a6a400ca742bf883e504e4355
dstanton <djstanton@gmail.com>
2021-03-27 16:05:07 -0700

    Initial commit

---
+++ src/com/djstanton/Calculator.java
@@ -0,0 +1,15 @@
+package com.djstanton;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return this.floor.getArea() * carpet.getCost();
+    }
+}
---
+++ src/com/djstanton/Floor.java
@@ -0,0 +1,15 @@
+package com.djstanton;
+
+public class Floor {
+    private double length;
+    private double width;
+
+    public Floor(double length, double width) {
+        this.length = (length < 0) ? 0: length;
+        this.width = (width < 0 ) ? 0: width;
+    }
+
+    public double getArea() {
+        return ((double)(double)this.length * (double)this.width);
+    }
+}
